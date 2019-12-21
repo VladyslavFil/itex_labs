1ce62b4f60a59f6d07b8054a1d191feb64cdefb9
VladyslavFil <58748168+VladyslavFil@users.noreply.github.com>
2019-12-21 13:35:10 +0300

    Add files via upload

---
+++ itex4/save.php
@@ -0,0 +1,9 @@
+<?php
+    $savedTest = htmlspecialchars($_POST['text']);
+
+    $file = fopen('textData.txt', 'w+');
+    fwrite($file, $savedTest);
+    fclose($file);
+
+    header('Location: index.php');
+?>
